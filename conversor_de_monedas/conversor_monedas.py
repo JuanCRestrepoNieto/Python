2f93899ab54449ce2c07aece9f9c96f550ecbf46
Totta <juanrestrepo300532@correo.itm.edu.co>
2022-11-03 09:00:27 -0500

    Conversor monedas

--- conversor_de_monedas/conversor_monedas.py
+++ conversor_de_monedas/conversor_monedas.py
@@ -1,3 +1,16 @@
-pesos = input("¿Cuánto dinero quieres cambiar a otra moneda? ")
-pesos = float(pesos)
+print("¿Qué deseas realizar?\n")
+print("Opción 1: De peso colombiano a Dolar\n")
+print("Opción 2: De dolar a peso colombiano\n")
+opcion = input()
 
+dinero = float(input("¿Cuánto dinero quieres cambiar?"))
+if(int(opcion) == 1):
+    valor_dolar = 4900
+    dolares = dinero / valor_dolar
+    print("Tienes en total "+ str(round(dolares, 2))+ " dólares")
+elif int(opcion)==2:
+    peso_colombiano = 1/4900
+    pesos_colombianos = dinero / peso_colombiano
+    print("Tienes en total "+ str(round(pesos_colombianos, 2))+ " pesos colombianos")
+else:
+    print("No escogiste ninguna de las opciones")
